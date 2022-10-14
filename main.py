aadb2212fae536cdce455df9a93f116d37359f69
ZakYeo <zakyeomanson@gmail.com>
2022-10-14 12:19:46 +0100

    Improved login functionality and added logged_in page

--- main.py
+++ main.py
@@ -1,26 +1,57 @@
-from flask import Flask, redirect, url_for, request, render_template
-app = Flask(__name__)
+from flask import Flask, redirect, url_for, request, render_template, session, flash
+from flask_bootstrap import Bootstrap5
+import os
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
 
 @app.route("/")
 def home():
-    return render_template('login.html')
+    if not session.get("logged_in"):
+        return render_template('login.html')
+    else:
+        return "You're logged in"
 
 
 @app.route('/success/<name>')
 def success(name):
-    return 'welcome %s' % name
+    if(is_logged_in()):
+        return render_template("logged_in.html", name=name)
+    else:
+        return home()
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        user = request.form['name']
-        return redirect(url_for('success', name=user))
+        user = request.form['username']
+        password = request.form['password']
+        if(user == "admin" and password == "admin"):
+            session["logged_in"] = user
+            return redirect(url_for('success', name=user))
+        else:
+            return home()
     else:
-        user = request.args.get('name')
-        return redirect(url_for('success', name=user))
+        if(is_logged_in()):
+            return redirect(url_for('success', name=session["logged_in"]))
+        else:
+            return render_template('login.html')
+
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = None
+    return home()
+
+
+def is_logged_in():
+    try:
+        _ = session["logged_in"]
+        return True
+    except KeyError:
+        return False
 
 
 if __name__ == '__main__':
+    app.secret_key = os.urandom(12)
+    Bootstrap5(app)
     app.run(debug=True)
